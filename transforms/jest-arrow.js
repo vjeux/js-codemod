78a1578d7d5e8b231bb85921cd886ccf3569a415
Christopher Chedeau <vjeux@fb.com>
2016-09-06 14:45:10 -0700

    Add normal to arrow function transform for jest tests

---
+++ transforms/jest-arrow.js
@@ -0,0 +1,31 @@
+export default function transformer(file, api) {
+  const j = api.jscodeshift;
+
+  const functionsToTransform = [
+    'describe',
+    'beforeEach',
+    'afterEach',
+    'it',
+    'xit',
+    'test',
+    'xdescribe',
+  ];
+
+  return j(file.source)
+    .find(j.ExpressionStatement)
+    .filter(path => {
+      return (
+        path.node.expression.type === 'CallExpression' &&
+        path.node.expression.callee.type === 'Identifier' &&
+        functionsToTransform.indexOf(path.node.expression.callee.name) !== -1
+      );
+    })
+    .forEach(path => {
+      var lastArg = path.node.expression.arguments.length - 1;
+      var fn = path.node.expression.arguments[lastArg];
+
+      path.node.expression.arguments[lastArg] = 
+        j.arrowFunctionExpression(fn.params, fn.body);
+    })
+    .toSource();
+}
